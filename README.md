c70a3a00f495023aa105adb4de40b571e889e524
FoundrySH <no-reply@foundry.sh>
2018-04-05 18:03:57 -0500

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# PostCSS Purgecss
+
+[PostCSS] plugin for PurgeCSS.
+
+[PostCSS]: https://github.com/postcss/postcss
+
+## Usage
+
+```js
+postcss([ require('postcss-purgecss') ])
+```
+
+See [PostCSS] docs for examples for your environment.
---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import babel from "rollup-plugin-babel"
+import builtins from "rollup-plugin-node-builtins"
+import resolve from "rollup-plugin-node-resolve"
+import flow from "rollup-plugin-flow"
+import uglify from "rollup-plugin-uglify"
+
+export default {
+    input: "src/index.js",
+    output: [
+        {
+            file: "index.js",
+            format: "cjs"
+        }
+    ],
+    plugins: [builtins(), resolve(), flow(), babel(), uglify()],
+    external: ["postcss", "postcss-selector-parser", "fs", "glob"]
+}
