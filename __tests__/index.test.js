1671cb1e5b007e363a67454cada8adf7aa68233f
Ffloriel <florielfedry@gmail.com>
2018-04-24 07:59:49 +0100

    add config file tests
    - add tests with valid and invalid config file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -27,4 +27,37 @@ describe('Purgecss postcss plugin', () => {
                 })
         })
     }
+
+    it('remove unused css with config file', done => {
+        const input = fs
+            .readFileSync(`${__dirname}/fixtures/src/simple/simple.css`)
+            .toString()
+        const expected = fs
+            .readFileSync(`${__dirname}/fixtures/expected/simple.css`)
+            .toString()
+        postcss([purgecss(`${__dirname}/fixtures/src/config/config.js`)])
+            .process(input)
+            .then(result => {
+                expect(result.css).toBe(expected)
+                expect(result.warnings().length).toBe(0)
+                done()
+            })
+    })
+
+    it('throws an error if the config file is not found', done => {
+        expect.assertions(1)
+        const input = fs
+            .readFileSync(`${__dirname}/fixtures/src/simple/simple.css`)
+            .toString()
+
+        postcss([purgecss(`wrongpath/config.js`)])
+            .process(input)
+            .then(() => {
+                done()
+            })
+            .catch(e => {
+                expect(e instanceof Error).toBe(true)
+                done()
+            })
+    })
 })
